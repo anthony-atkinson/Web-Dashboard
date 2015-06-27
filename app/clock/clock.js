8fbd46be68c2b5324cc57b55ce820c5a768984b1
Anthony <anthony@anthonyatkinson.info>
2015-06-27 14:21:22 -0600

    Updating the project: 1. Some CSS updates. 2. Changed the clock view page a lot. 3. Added JS functions

--- app/clock/clock.js
+++ app/clock/clock.js
@@ -9,6 +9,18 @@ angular.module('myApp.clock', ['ngRoute'])
   });
 }])
 
-.controller('clock', [function() {
+.controller('clock', ['$scope', '$http', function($scope, $http) {
+      //$scope.bgPicture = {};
+      $scope.bgPicture = '/images/backgrounds/default.jpg';
 
+      $scope.changeBgPicture = function() {
+        //$.ajax({url: "http://pi.anthonyatkinson.info/getRandomBackgroundImageUrl.php", success: function(result){
+        //  $scope.bgPicture = result;
+        //}});
+        $http.jsonp('http://pi.anthonyatkinson.info/getRandomBackgroundImageUrl.php?callback=JSON_CALLBACK').
+            success(function(data) {
+              $scope.bgPicture = data.image;
+              console.log(data.image);
+            });
+      };
 }]);
