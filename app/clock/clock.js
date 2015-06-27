7e2ee75d7bfd8ecf75dcce9a126b8dd48f9ea636
Anthony <anthony@anthonyatkinson.info>
2015-06-27 15:45:35 -0600

    1. Made the clock display right (for a 1080p screen. not sure on others..). 2. Added the background change timer and start and stop functions for it.

--- app/clock/clock.js
+++ app/clock/clock.js
@@ -9,18 +9,50 @@ angular.module('myApp.clock', ['ngRoute'])
   });
 }])
 
-.controller('clock', ['$scope', '$http', function($scope, $http) {
-      //$scope.bgPicture = {};
-      $scope.bgPicture = '/images/backgrounds/default.jpg';
+.controller('clock', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
+    $scope.bgPicture = '/images/backgrounds/default.jpg';
+    $scope.toggleBackground_button = "Pause Slideshow";
+    $scope.clockTime = "15:00";
 
-      $scope.changeBgPicture = function() {
-        //$.ajax({url: "http://pi.anthonyatkinson.info/getRandomBackgroundImageUrl.php", success: function(result){
-        //  $scope.bgPicture = result;
-        //}});
+    $scope.changeBgPicture = function() {
         $http.jsonp('http://pi.anthonyatkinson.info/getRandomBackgroundImageUrl.php?callback=JSON_CALLBACK').
             success(function(data) {
               $scope.bgPicture = data.image;
-              console.log(data.image);
-            });
-      };
+            }
+        );
+    };
+
+    var bgTimer = undefined;
+
+    $scope.stopBgTimer = function(){
+        if(angular.isDefined(bgTimer)) {
+            $interval.cancel(bgTimer);
+            bgTimer = undefined;
+            return true;
+        }
+        return false;
+    };
+
+    $scope.startBgTimer = function() {
+        // Don't start a new timer if we are already have one running
+        if ( angular.isDefined(bgTimer) ) {
+            return false;
+        }
+        bgTimer = $interval(function(){
+            $scope.changeBgPicture();
+        },10000);
+        return true;
+    };
+
+    $scope.toggleBackground_change = function() {
+        if($scope.startBgTimer() ) {
+            $scope.toggleBackground_button = "Pause Slideshow";
+        }else {
+            $scope.stopBgTimer();
+            $scope.toggleBackground_button = "Start Slideshow";
+        }
+    };
+
+    $scope.changeBgPicture();
+    $scope.startBgTimer();
 }]);
