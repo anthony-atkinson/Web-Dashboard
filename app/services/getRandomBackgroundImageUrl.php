4968823097973ed853389e887b8d350eb0cd4fc8
root <root@ubuntu2.anthonyatkinson.info>
2015-10-17 01:36:53 -0400

    Updating the random background service to return the image encoded in base64

--- app/services/getRandomBackgroundImageUrl.php
+++ app/services/getRandomBackgroundImageUrl.php
@@ -15,12 +15,25 @@
 	
 	$imageurl = "http://{$_SERVER['SERVER_NAME']}/images/" . rawurlencode(substr($files[$file], strlen($dir)+1) );
 	// $json = {'{$imageurl}'};
+	$data = file_get_contents($files[$file]);
+	$type = pathinfo($files[$file], PATHINFO_EXTENSION);
 	$json = array(
-        'image' => $imageurl
+        'imageURL' => $imageurl,
+				'encodedImage' => encodeImageToBase64($files[$file])
 	);
 	$json_str = json_encode($json);
 	// echo $json_str;
 	//header("Content-Type: application/json");
-    echo $_GET['callback'] . '(' . "{'image' : '{$imageurl}'}" . ')';
+    // echo $_GET['callback'] . '(' . "{'image' : '{$imageurl}'}" . ')';
+    echo $_GET['callback'] . '(' . json_encode($json) . ')';
 	// echo $imageurl;
+	
+	function encodeImageToBase64($path) {
+		$data = file_get_contents($path);
+		$type = pathinfo($path, PATHINFO_EXTENSION);
+		$image = base64_encode($data);
+		return array(
+			'data:image/' . $type . ';base64,' . $image
+		);
+	}
 ?>
