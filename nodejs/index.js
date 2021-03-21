ffb7cc97b93911dfc18028e9370181e1347bb9cb
Anthony Atkinson <anthonyatkinson1@gmail.com>
2021-03-21 01:22:28 -0600

    Updating the endpoint that returns files so that it returns the right response

--- nodejs/index.js
+++ nodejs/index.js
@@ -18,7 +18,7 @@ app.get('/random_background', (req, res) => {
   glob("public/images/backgrounds/**/*", (err, files) => {
     if (files && files.length > 0) {
       const fileIndex = Math.floor(Math.random() * files.length);
-      res.send(files[fileIndex].replace("public/", ""));
+      res.send({ image: files[fileIndex].replace("public/", "") });
     } else {
       res.status(500).send('no files found');
     }
